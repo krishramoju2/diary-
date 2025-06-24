9cbb571eae8eb95a15077193c51dbbeef7417067
krishramoju2 <futureai.incorporated@gmail.com>
2025-06-24 23:40:42 +0530

    Create JournalTimeline.js

---
+++ src/JournalTimeline.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const JournalTimeline = () => (
+  <div>
+    <h2>Timeline (Coming Soon)</h2>
+    <p>This will show your past entries.</p>
+  </div>
+);
+
+export default JournalTimeline;
